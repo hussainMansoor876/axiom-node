d7f7d8a0630a3a9dc4dca31edf78512fcd432f10
mansoor876 <mansoor.hussain@mach3bi.com>
2021-10-31 15:33:40 +0500

    done

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const PORT = 8080
 app.use(express.json())
 app.use(cors())
 
-// app.get('/', (req, res) => {
-//     try {
-//         return res.send({ message: 'Hello Nodejs', success: true })
-//     }
-//     catch (e) {
-//         console.log('e', e)
-//         return res.send({ message: e?.message, success: false })
-//     }
-// })
+app.get('/', (req, res) => {
+    try {
+        return res.send({ message: 'Hello Nodejs Server is Running!', success: true })
+    }
+    catch (e) {
+        console.log('e', e)
+        return res.send({ message: e?.message, success: false })
+    }
+})
 
 // app.post('/login', (req, res) => {
 //     try {
